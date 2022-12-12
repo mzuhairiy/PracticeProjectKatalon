f86615da82af077972343b197848cc2a792ef1db
atasnamazuhair@gmail.com <zuhairimuhammad13@gmail.com>
2022-12-12 16:54:56 +0700

    create test suite and test suite collection

---
+++ Test Suites/Login/TS_Login.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cdde9b02-0286-41f3-b84f-21c2ecc014e3</testSuiteGuid>
+   <testCaseLink>
+      <guid>af75f5f8-2532-4c61-8ea7-a39207076690</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1_ValidLogin</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>96fcbb14-56d0-4a9e-8f81-76c7b87fb83d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC2_InvalidUsernameLogin</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f2e4ecaa-9d80-4649-8349-9af6f4be5edf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC3_InvalidPasswordLogin</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ece534b6-6fec-41a7-b3cc-3767956b9390</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC4_NullUsernameLogin</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4e335baa-bce7-4a69-9706-d352da7f2949</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC5_NullPasswordLogin</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>54778d08-144d-4051-b04d-b5fdf6932dd9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC6_NullUsernameAndPassword</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
